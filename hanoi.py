a9e7e0f73852afec4dcd9a65826337f1314bacf0
psharratt <psharratt1@gmail.com>
2023-02-26 14:59:01 +0100

    added comments to hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -2,22 +2,30 @@
 """
 Created on Mon Feb 20 08:24:20 2023
 
-@author: paulsharratt
+@authors: Fabian Metz | Amin Oueslati | Oskar Krafft | Paul Sharratt
 
-Base case/simplest case: moving one disk to destination rod/to_rod, i.e. a tower of one disk and only one move.
-1. move n - 1 from from_rod to aux_rod
-2. move n from from_rod to to_rod
-3. move n - 1 from aux_rod to to_rod.
+Assumptions: 
+
+- Base case/simplest case: moving one disk to destination rod/to_rod, i.e. a tower of one disk and only one move.
+
+- Function requires three moves, two of which are recursive calls:
+    
+        1. move n - 1 from from_rod to aux_rod
+        2. move n from from_rod to to_rod
+        3. move n - 1 from aux_rod to to_rod.
 
 """
 
 def TowerOfHanoi(n, from_rod, to_rod, aux_rod): 
-    if n == 0: #base case
+    if n == 0: #no more disks
         return
     TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) # 1st recursive call
-    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
-    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)# 2nd recursive call, reordering of rods
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod) # Moving the disk from from_rod to to_rod
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)# 2nd recursive call: moving remaining disk from aux to to_rod using from_rod.
 
 
 TowerOfHanoi(3, 'A', 'C', 'B')  
 
+"""
+
+"""
