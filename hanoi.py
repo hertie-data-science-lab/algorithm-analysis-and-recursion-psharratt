9650295fc0275820696ee5e36c7cbd4aa1637148
psharratt <psharratt1@gmail.com>
2023-02-26 21:48:16 +0100

    updated notes, complexity, and added counter

--- hanoi.py
+++ hanoi.py
@@ -13,17 +13,30 @@ Assumptions:
         1. move n - 1 from from_rod to aux_rod
         2. move n from from_rod to to_rod
         3. move n - 1 from aux_rod to to_rod.
+        
+Notes: 
+
+- The function has 2^n-1 time complexity. We tested this with the move counter.
 
 """
+# Move counter init for tracking complexity
+MoveCount = 0 
 
 def TowerOfHanoi(n, from_rod, to_rod, aux_rod): 
-    if n == 0: #no more disks
+    # Base case
+    if n == 0: 
         return
-    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) # 1st recursive call: moving the n-1 disk from from_rod aux using to_rod
-    print("Move disk", n, "from rod", from_rod, "to rod", to_rod) # Moving the disk from from_rod to to_rod
-    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)# 2nd recursive call: moving remaining disk from aux to to_rod using from_rod.
+    # Counter for moves
+    global MoveCount
+    MoveCount += 1 
+    # 1st recursive call: moving the n-1 disk from from_rod aux using to_rod
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
+    # Moving the disk from from_rod to to_rod
+    print("Disk", n, "moves from rod", from_rod, "to rod", to_rod) 
+    # 2nd recursive call: moving remaining disk from aux to to_rod using from_rod.
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
 
 
 TowerOfHanoi(3, 'A', 'C', 'B')  
 
-
+print("Total moves:", MoveCount)
